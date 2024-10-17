0f9009ffc12f2126331c1e3aeabee97e3ef2e4fd
CBX <chenbingxu369@gmail.com>
2024-10-17 20:40:50 +0800

    changed application name

--- IntelliEdu-application-service/src/main/java/com/team6/intellieduapplicationservice/AppServiceApplication.java
+++ IntelliEdu-application-service/src/main/java/com/team6/intellieduapplicationservice/AppServiceApplication.java
@@ -1,4 +1,4 @@
-package com.team6.intellieduscoringservice;
+package com.team6.intellieduapplicationservice;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication()
-@MapperScan("com.team6.intellieduscoringservice.mapper")
+@MapperScan("com.team6.intellieduapplicationservice.mapper")
 @EnableFeignClients(basePackages = "com.team6.intellieduapi.client")
-public class MainApplication {
+public class AppServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(AppServiceApplication.class, args);
     }
 
 }
--- IntelliEdu-user-service/src/main/java/com/team6/intellieduuserservice/UserServiceApplication.java
+++ IntelliEdu-user-service/src/main/java/com/team6/intellieduuserservice/UserServiceApplication.java
@@ -1,17 +1,19 @@
-package com.team6.intellieduapplicationservice;
+package com.team6.intellieduuserservice;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication()
-@MapperScan("com.team6.intellieduapplicationservice.mapper")
+@MapperScan("com.team6.intellieduuserservice.mapper")
 @EnableFeignClients(basePackages = "com.team6.intellieduapi.client")
-public class MainApplication {
+@EnableDiscoveryClient
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 }
