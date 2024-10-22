0ea8dd9f721bd10f761f9bff1966ff0cdbc5a065
CBX <chenbingxu369@gmail.com>
2024-10-22 20:01:46 +0800

    fix: integrate ai into Common module

--- IntelliEdu-application-service/src/main/java/com/team6/intellieduapplicationservice/AppServiceApplication.java
+++ IntelliEdu-application-service/src/main/java/com/team6/intellieduapplicationservice/AppServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication()
+@SpringBootApplication(scanBasePackages = {"com.team6.intellieduapplicationservice", "com.team6.intellieducommon"})
 @MapperScan("com.team6.intellieduapplicationservice.mapper")
 @EnableFeignClients(basePackages = "com.team6.intellieduapi.client")
 public class AppServiceApplication {
